d5ee4b81f6fa7c9e3fca2f6d0326c3000b9baafb
co2co2 <coco8287@hotmail.com>
2018-01-08 20:35:37 -0500

    take latest schema instead

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20180108192713) do
+ActiveRecord::Schema.define(version: 20180108210317) do
 
 
   create_table "ingredients", force: :cascade do |t|
