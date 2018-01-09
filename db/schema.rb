ca99d3e6574393df60ea13d7dac01fd2706ae92d
co2co2 <coco8287@hotmail.com>
2018-01-08 20:48:33 -0500

    merge from master

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180108210317) do
-
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
