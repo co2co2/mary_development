8dae167fe9491ec5b2581cf3fab9e34a0661f5f0
Situ William <situ_william@yahoo.com>
2018-01-10 17:35:40 -0500

    changed more queries to be inside models instead of controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
-		@favourites = @user.favourites
-		@recipes = @user.recipes
-		@fav_recipes = []
-		@favourites.each do |favourite|
-			@fav_recipes << Recipe.find(favourite.recipe_id)
-		end
+		@fav_recipes = Recipe.user_favourites(params[:id])	
 	end
 end
--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,4 +1,6 @@
 class Favourite < ApplicationRecord
 	belongs_to :user
 	belongs_to :recipe
+
+	scope :user_favourites, -> {joins(:recipe).where("favourites.user_id IS 1")}
 end
