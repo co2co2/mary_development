5211a1a5b4ec445e8a29d89258db9a8b9b558ac0
Lena <fatmeow@gmail.com>
2018-01-11 18:28:55 -0500

    Fix add strain field form able to save strains

--- app/controllers/strains_controller.rb
+++ app/controllers/strains_controller.rb
@@ -1,6 +1,11 @@
 class StrainsController < ApplicationController
-	def index
-		@strains = Strain.order(:name).where("name LIKE ?", "%#{params[:term]}%")
-		render json: @strains.map(&:name)
-	end
+  def index
+        @strains = Strain.order(:name).where("name LIKE ?", "%#{params[:term]}%")
+        strain_hash = {}
+        @strains.each do |strain|
+        strain_hash[strain.id] = strain.name
+        end
+        render json: strain_hash
+
+    end
 end
