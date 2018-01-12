ca78e3af4f9435b53b9c5ba21e515ea82a7886bb
Lena <fatmeow@gmail.com>
2018-01-11 19:27:20 -0500

    Fix Strain field glitch by moving js to form page

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -1,27 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  $(function(){
-    $('#recipe_strain_name').focusout(function(){
-      var strainText = $('#recipe_strain_name').val();
-      console.log(strainText)
-      $.ajax({
-        url: $('#recipe_strain_name').data('autocomplete-source'),
-        method: 'GET',
-        dataType: 'json'
-      }).done(function(responseData) {
-        for (var key in responseData)
-        {
-          if ( responseData[key].toLowerCase() === strainText.toLowerCase()) {
-            console.log(`strain id: ${key}`)
-            $('#strainId').val(key);
-          }
 
-        }
-      }).fail(function() {
-        console.log(`fail to get strains info`)
-      })
-
-    })
-  })
 })
 
 function hideConcentrateLink() {
@@ -36,9 +14,3 @@ function hideConcentrateLink() {
 function addConcentrateLink() {
   addConcentrate.style.display = 'inline';
 }
-
-jQuery(function() {
-  return $('#recipe_strain_name').autocomplete({
-    source: $('#recipe_strain_name').data('autocomplete-source')
-  });
-});
