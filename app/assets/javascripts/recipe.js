098b9f223fd09769a82ca42d8557a0c6cb88d21c
Situ William <situ_william@yahoo.com>
2018-01-12 10:43:51 -0500

    fixed some inline scripts, and added seed data for allergies

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -1,6 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(document).on('turbolinks:load', function() {
 
-})
+  $('#recipe_strain_name').focusout(function(){
+    var strainText = $('#recipe_strain_name').val();
+    $.ajax({
+      url: $('#recipe_strain_name').data('autocomplete-source'),
+      method: 'GET',
+      dataType: 'json'
+    }).done(function(responseData) {
+      for (var key in responseData)
+      {
+        if ( responseData[key].toLowerCase() === strainText.toLowerCase()) {
+          $('#strainId').val(key);
+        }
+      }
+    }).fail(function() {
+      console.log(`fail to get strains info`)
+    })
+
+  })
+
+  $('#recipe_strain_name').autocomplete({
+  source: $('#recipe_strain_name').data('autocomplete-source')
+});
+
+});
 
 function hideConcentrateLink() {
   var addConcentrate = document.querySelector('a#addConcentrate')
