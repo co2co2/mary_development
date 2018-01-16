c7fd8630816dbea758ada37a8ada98f07ae63e8a
Lena <fatmeow@gmail.com>
2018-01-16 13:00:04 -0500

    trying to figureout some bootstrap stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
+//= require bootstrap
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
