2b456d32d8a77a03645a4535df61b2cc5c4979e5
Lena <fatmeow@gmail.com>
2018-01-26 00:40:32 -0500

    Add method for handling not found errors in static pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
   def show
-    render template: "pages/#{params[:page]}"
+    if valid_page?
+      render template: "pages/#{params[:page]}"
+    else # Handles not found errors
+      render file: "public/404.html", status: :not_found
+    end
+  end
+
+  private
+  # A method that checks page exists - returns boolean
+  def valid_page?
+    File.exist?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb"))
   end
 end
